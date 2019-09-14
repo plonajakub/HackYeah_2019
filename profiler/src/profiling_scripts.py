07fa37a3e285321c4d7396ebe388658684b34070
Jakub Plona <plonajakub@gmail.com>
2019-09-14 19:17:57 +0200

    Create skeleton for the profiling algorithm

---
+++ profiler/src/profiling_scripts.py
@@ -0,0 +1,32 @@
+
+class Profiler:
+
+    def __init__(self):
+        # JSONs
+        user_history = None
+        articles = None
+
+    def parse_jsons(self, path_user_history, path_articles):
+        """Fills user_history and articles with data"""
+
+    def create_tag_distribution_from_user_history(self):
+        """Counts tags in user_history"""
+        tag_dictionary = {}
+        return tag_dictionary
+
+    def find_matching_articles(self, tag_distribution):
+        """Searches for best matched articles comparing tags"""
+        articles_url = []
+        return articles_url
+
+    def print_matching_articles_urls(self, articles_url):
+        """Export URLs for matched articles to the STDOUT"""
+        print(articles_url)
+
+    def run(self, path_user_history, path_articles):
+        """Algorithm"""
+        self.parse_jsons(path_user_history, path_articles)
+        tag_distribution = self.create_tag_distribution_from_user_history()
+        articles_url = self.find_matching_articles(tag_distribution)
+        self.print_matching_articles_urls(articles_url)
+
