e2252609522cc725457906a3058054afc95768b3
Jakub Plona <plonajakub@gmail.com>
2019-09-14 19:58:56 +0200

    Fix paths to JSONs

--- profiler/src/main.py
+++ profiler/src/main.py
@@ -1,5 +1,5 @@
 from src import profiling_scripts as ps
 
 profiler = ps.Profiler()
-profiler.run('../user_history.json', '../articles.json')
+profiler.run('../data/user_history.json', '../data/articles.json')
 
